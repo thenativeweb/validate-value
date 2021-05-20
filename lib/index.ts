b22ff54368399e85dfd929dfec0364741cc47d55
Golo Roden <golo.roden@thenativeweb.io>
2021-05-20 14:19:32 +0200

    fix: Export ParseError.

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,8 @@
 import { isOfType } from './isOfType';
 import { JSONSchema7 as JsonSchema } from 'json-schema';
 import { parse } from './parse';
+import { ParseError } from './ParseError';
 import { Parser } from './Parser';
 
-export type { JsonSchema };
+export type { JsonSchema, ParseError };
 export { isOfType, parse, Parser };
