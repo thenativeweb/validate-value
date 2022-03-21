bce807fea4beac2114d05c43d4e6dff68d5b282a
Noah Hummel <noah.hummel@thenativeweb.io>
2022-03-21 13:33:47 +0100

    chore: improve unit test for getDefaultAjvInstance (#420)

--- test/unit/getDefaultAjvInstanceTests.ts
+++ test/unit/getDefaultAjvInstanceTests.ts
@@ -1,17 +1,73 @@
 import { assert } from 'assertthat';
-import { getDefaultAjvInstance } from '../../lib/getDefaultAjvInstance';
+import { isCustomError } from 'defekt';
+import { getDefaultAjvInstance, ParseError, Parser } from '../../lib';
 
 suite('getDefaultAjvInstance', (): void => {
   test('allows union types.', async (): Promise<void> => {
     const defaultAjvInstance = getDefaultAjvInstance();
+    const parser = new Parser({
+      type: [ 'string', 'number' ]
+    }, {
+      ajvInstance: defaultAjvInstance
+    });
 
-    assert.that(defaultAjvInstance.opts.allowUnionTypes).is.true();
+    assert.that((): void => {
+      parser.parse(null).unwrapOrThrow();
+    }).is.throwing(
+      (ex): boolean => isCustomError(ex) && ex.code === ParseError.code
+    );
+    assert.that((): void => {
+      parser.parse([ false ]).unwrapOrThrow();
+    }).is.throwing(
+      (ex): boolean => isCustomError(ex) && ex.code === ParseError.code
+    );
+    assert.that((): void => {
+      parser.parse({ foo: 'bar' }).unwrapOrThrow();
+    }).is.throwing(
+      (ex): boolean => isCustomError(ex) && ex.code === ParseError.code
+    );
+
+    assert.that((): void => {
+      parser.parse('foobar').unwrapOrThrow();
+    }).is.not.throwing();
+    assert.that((): void => {
+      parser.parse(1_337).unwrapOrThrow();
+    }).is.not.throwing();
   });
 
-  test('adds support for alphanumeric format.', async (): Promise<void> => {
+  test('supports the "alphanumeric" format.', async (): Promise<void> => {
     const defaultAjvInstance = getDefaultAjvInstance();
-    const alphanumericFormat = defaultAjvInstance.formats.alphanumeric;
+    const parser = new Parser({
+      type: 'string',
+      format: 'alphanumeric'
+    }, {
+      ajvInstance: defaultAjvInstance
+    });
 
-    assert.that(alphanumericFormat).is.not.undefined();
+    assert.that((): void => {
+      parser.parse('$%&').unwrapOrThrow();
+    }).is.throwing(
+      (ex): boolean => isCustomError(ex) && ex.code === ParseError.code
+    );
+    assert.that((): void => {
+      parser.parse('').unwrapOrThrow();
+    }).is.throwing(
+      (ex): boolean => isCustomError(ex) && ex.code === ParseError.code
+    );
+    assert.that((): void => {
+      parser.parse(null).unwrapOrThrow();
+    }).is.throwing(
+      (ex): boolean => isCustomError(ex) && ex.code === ParseError.code
+    );
+
+    assert.that((): void => {
+      parser.parse('foobar').unwrapOrThrow();
+    }).is.not.throwing();
+    assert.that((): void => {
+      parser.parse('1_337').unwrapOrThrow();
+    }).is.not.throwing();
+    assert.that((): void => {
+      parser.parse('foobar1_337').unwrapOrThrow();
+    }).is.not.throwing();
   });
 });
